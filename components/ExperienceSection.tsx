ac3074e9e2f6b752c3b266cda9a06db6d52bdee5
Cyrus <c_camanatad@unilab.com.ph>
2025-10-25 22:54:07 +0800

    update

--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -17,11 +17,11 @@ const TimelineItem: React.FC<TimelineItemProps> = ({ experience }) => (
       </svg>
     </span>
     <div className="bg-navy-light text-justify p-6 rounded-lg shadow-lg">
-      <div className="flex flex-col justify-center md:justify-start md:flex-row items-start mb-4">
-        <h3 className="mb-1 text-xl font-semibold text-white">
+      <div className="flex flex-col justify-center md:justify-start md:flex-row md:items-start mb-4">
+        <h3 className="mb-1 text-xl text-center font-semibold text-white">
           {experience.role} 
         </h3>
-        <span className="ml-2 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full bg-blue-900 border border-blue-400">
+        <span className="ml-2 text-center text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full bg-blue-900 border border-blue-400">
           {experience.company}
         </span>
       </div>
